7d09b33afedbbfc9c5bdce5de37bb6cd5d95a879
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-01-07 17:07:16 -0800

    Implemented testcases for mostCommonSpaceEfficient method

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -6,7 +6,7 @@ public class PracticeTest {
   
   // TODO: Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
   @Test
-  void testmostCommonTimeEfficient() {
+  void testMostCommonTimeEfficient() {
     //Arrange
     int[] nums = {2, 1, 2, 3, 1, 2};
     //Act
@@ -16,9 +16,9 @@ public class PracticeTest {
   }
 
   @Test
-  void testmostCommonTimeEfficientAllSameNumbers() {
+  void testMostCommonTimeEfficientAllSameNumbers() {
     //Arrange
-    int[] nums = {1,1,1,1,1,1,};
+    int[] nums = {1,1,1,1,1,1};
     //Act
     int actual = Practice.mostCommonTimeEfficient(nums);
     //Assert
@@ -26,9 +26,9 @@ public class PracticeTest {
   }
 
   @Test
-  void testmostCommonTimeEfficientWithNegatives() {
+  void testMostCommonTimeEfficientWithNegatives() {
     //Arrange
-    int[] nums = {-1, -1, -1, -1, 5, 1,};
+    int[] nums = {-1, -1, -1, -1, 5, 1};
     //Act
     int actual = Practice.mostCommonTimeEfficient(nums);
     //Assert
@@ -36,13 +36,54 @@ public class PracticeTest {
   }
 
   @Test
-  void testmostCommonTimeEfficientWithLargeNumbers() {
+  void testMostCommonTimeEfficientWithLargeNumbers() {
     //Arrange
     int[] nums = {1, 10, 100, 1000, 1000, 1000};
     //Act
     int actual = Practice.mostCommonTimeEfficient(nums);
     //Assert
     assertEquals(1000, actual);
+  }
+  
+  //Practice.mostCommonSpaceEfficient
+  @Test
+  void testMostCommonSpaceEfficient() {
+    //Arrange
+    int[] nums = {1,2,3,4,5,5};
+    //Act
+    int actual = Practice.mostCommonSpaceEfficient(nums);
+    //Assert
+    assertEquals(5, actual);
+  }
+
+  @Test
+  void testMostCommonSpaceEfficientAllSameNumbersNegative() {
+    //Arrange
+    int[] nums = {-1,-1,-1,-1,-1,-1};
+    //Act
+    int actual = Practice.mostCommonSpaceEfficient(nums);
+    //Assert
+    assertEquals(-1, actual);
+  }
+
+  @Test
+  void testMostCommonSpaceEfficientAllSameNumbersPostive() {
+    //Arrange
+    int[] nums = {5,5,5,5,5};
+    //Act
+    int actual = Practice.mostCommonSpaceEfficient(nums);
+    //Assert
+    assertEquals(5, actual);
+  }
+
+   @Test
+  void testMostCommonSpaceEfficientWithLargeNumbers() {
+    //Arrange
+    int[] nums = {1, 10, 10, 1000, 1000, 10};
+    //Act
+    int actual = Practice.mostCommonSpaceEfficient(nums);
+    //Assert
+    assertEquals(10, actual);
   }
 
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
