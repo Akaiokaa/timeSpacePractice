d60e130a9ceebb5bc3661866b76397aaf4826ac8
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-01-07 16:39:39 -0800

    Added test cases for testMostCommonTimeEfficient

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -5,6 +5,45 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PracticeTest {
   
   // TODO: Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
+  @Test
+  void testmostCommonTimeEfficient() {
+    //Arrange
+    int[] nums = {2, 1, 2, 3, 1, 2};
+    //Act
+    int actual = Practice.mostCommonTimeEfficient(nums);
+    //Assert
+    assertEquals(2, actual);
+  }
+
+  @Test
+  void testmostCommonTimeEfficientAllSameNumbers() {
+    //Arrange
+    int[] nums = {1,1,1,1,1,1,};
+    //Act
+    int actual = Practice.mostCommonTimeEfficient(nums);
+    //Assert
+    assertEquals(1, actual);
+  }
+
+  @Test
+  void testmostCommonTimeEfficientWithNegatives() {
+    //Arrange
+    int[] nums = {-1, -1, -1, -1, 5, 1,};
+    //Act
+    int actual = Practice.mostCommonTimeEfficient(nums);
+    //Assert
+    assertEquals(-1, actual);
+  }
+
+  @Test
+  void testmostCommonTimeEfficientWithLargeNumbers() {
+    //Arrange
+    int[] nums = {1, 10, 100, 1000, 1000, 1000};
+    //Act
+    int actual = Practice.mostCommonTimeEfficient(nums);
+    //Assert
+    assertEquals(1000, actual);
+  }
 
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
 }
